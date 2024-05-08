5bfd2656bb6e432a3fa381b5a017d85dd70b31c5
Mohamed Ibrahim | al-reyadah <mo7mad369@gmail.com>
2024-05-08 17:26:23 +0300

    70% from services page

--- src/app/(pages)/services/page.js
+++ src/app/(pages)/services/page.js
@@ -1,12 +1,14 @@
 import PageDiscription from "~/components/servicesPage/Description";
 import Services from "~/components/servicesPage/Services";
 import Footer from "~/components/homepage/Footer";
+import More from "~/components/servicesPage/More";
 
 export default function Services_Page() {
   return (
     <>
       <PageDiscription />
       <Services />
+      <More />
       <Footer />
     </>
   );
---
+++ src/components/servicesPage/More/index.js
@@ -0,0 +1,5 @@
+import css from "./style.module.scss";
+
+export default function More() {
+  return <div className={`${css.more} mil-p-40`}></div>;
+}
